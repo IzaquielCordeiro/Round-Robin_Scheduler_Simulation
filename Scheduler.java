e56654caab988134ef8670ffa5ca4c08b3004581
IzaquielCordeiro <izaquiel.cordeiro@ccc.ufcg.edu.br>
2020-10-10 17:49:31 -0300

    Processes will now be blocked when their time slice ends

--- Scheduler.java
+++ Scheduler.java
@@ -1,16 +1,16 @@
 public class Scheduler
 {
-    public static int N_PROCESS = 4;
-    public static int[] PROCESSES_TIME = {3000, 9000, 4500, 6000};
-    public static int N_THREADS= 0;
-    public static int EXECUTION_TIME = 2100;
-    public static int PROCESS_TIME = 2000;
-    public static int THREAD_TIME = 10000;
-    private static Queue<Flow> FLOWS = new Queue(N_PROCESS+N_THREADS);
+    static int N_PROCESS = 4;
+    static int[] PROCESSES_TIME = {3000, 9000, 4500, 6000};
+    static int N_THREADS= 0;
+    static int EXECUTION_TIME = 2100;
+    static int PROCESS_TIME = 2000;
+    static int THREAD_TIME = 10000;
+    static Queue<Flow> FLOWS = new Queue(N_PROCESS+N_THREADS);
 
-    private static Flow ExecutingFlow;
+    static Flow ExecutingFlow;
 
-    public static void main(String[] args) throws InterruptedException
+    public static void main(String[] args)
     {
         for (int i=1; i<=N_PROCESS; i++){FLOWS.add(new Process(i, PROCESSES_TIME[i-1]));}
         //for (int i=0; i<=(N_THREADS); i++){FLOWS.set(i, new Thread(1, 1000));}
